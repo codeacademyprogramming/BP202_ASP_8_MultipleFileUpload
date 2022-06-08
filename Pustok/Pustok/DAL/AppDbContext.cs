7e3cd6e11d4d5a53e417e059c28db1fa28c4b65f
eaghayev <eaghayevv@gmail.com>
2022-06-08 21:22:52 +0400

    project added

---
+++ Pustok/Pustok/DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pustok.Models;
+
+namespace Pustok.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookImage> BookImages { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+    }
+
+}
